687cc884cf5d849494123f9651d5a3b185a72978
Tanu Tarathikhampor <qewtanu@hotmail.com>
2023-11-29 01:53:45 +0700

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Qew-Tanu/',
   plugins: [react()],
-  resolve:{
-    alias:[{find:'@',replacement:'/src'}],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
   },
 })
