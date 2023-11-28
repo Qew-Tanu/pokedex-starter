219e00843ea22d35359352dbff362227dac5d8be
Tanu Tarathikhampor <qewtanu@hotmail.com>
2023-11-29 02:02:10 +0700

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Qew-Tanu/',
+  base: '/pokedex-starter/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
